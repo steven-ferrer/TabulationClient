b1f94d3e13dc3af4fa0e806c92ccb2527722536a
dovahkiin16 <edenfernandezjr@gmail.com>
2017-03-26 15:59:46 +0800

    Added vote page

---
+++ Tabulation/Event.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Tabulation
+{
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Event : Page
+    {
+        private static string _choiceId;
+        public static string choiceID
+        {
+            get { return _choiceId; }
+            set { _choiceId = value; }
+        }
+        private ObservableCollection<EventListItem> dataList = new ObservableCollection<EventListItem>();
+        public Event()
+        {
+            this.InitializeComponent();
+            initEventList();
+            EventList.ItemsSource = dataList;
+            
+        }
+
+        private void initEventList()
+        {
+            dataList.Add( new EventListItem() { EventID = "1.", EventName = "Cheershit"} );
+        }
+
+        public void ItemIsClicked(object sender, ItemClickEventArgs e )
+        {
+            EventListItem item = (EventListItem) e.ClickedItem;
+            choiceID = item.EventID;
+            this.Frame.Navigate(typeof(Voting));
+        }
+
+
+    }
+
+    public class EventListItem
+    {
+        public string EventName { get; set; }
+        public string EventID { get; set; }
+    }
+}
---
+++ Tabulation/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Text.RegularExpressions;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Tabulation
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private static string _serverAddress;
+        public static string _ServerAddress
+        {
+            get { return _serverAddress; }
+            set { _serverAddress = value; }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        public void setServerAddress(object sender, RoutedEventArgs e)
+        {
+            _ServerAddress = tb_serverAdd.Text;
+            this.Frame.Navigate(typeof(Event));
+        }
+    }
+}
